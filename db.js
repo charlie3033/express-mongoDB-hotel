a12f7ba19572fc47feae0e12b8759cca29e2951e
jari <mohitjari09@gmail.com>
2025-09-26 00:34:00 +0530

    fsdvsdv

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
-
+require('dotenv').config();
 //connection
-const mongoURL = 'mongodb://127.0.0.1:27017/hotels' 
+const mongoURL = process.env.MONGODB_URL;
 
 mongoose.connect(mongoURL,{
   useNewUrlParser: true,
--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const app = express();
 require('./db');
+require('dotenv').config();
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
-
+const PORT = process.env.PORT || 3000;
 
 app.set('json spaces',2)
-
-const port=3000;
 
 app.get('/',(req,res)=>{
   res.send('hello world');
@@ -22,6 +21,7 @@ const menuRoutes = require('./routes/menuRoutes');
 app.use('/menuItem',menuRoutes);
 
 
-app.listen(port,()=>{
-  console.log(`Server is listening at ${port}`)
+
+app.listen(PORT,()=>{
+  console.log(`Server is listening at ${PORT}`)
 });
